ad697a6b749b2aac865d5e59534a6344fd89f75d
Vincent Maurel <vincentmaurel82@gmail.com>
2018-12-16 15:49:04 -0500

    fix

--- startup/routes.js
+++ startup/routes.js
@@ -5,7 +5,7 @@ const error = require("../middleware/error");
 module.exports = function(app) {
   app.use(express.json());
   const cors = require("cors");
-  app.use(cors({ origin: "https://poc-react-member-search.herokuapp.com/" }));
+  app.use(cors({ origin: "https://poc-react-member-search.herokuapp.com" }));
   app.use("/api/members", members);
   app.use(error);
 };
