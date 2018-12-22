d01d92c262782a6622abd339791ac8eb65b987cc
Vincent Maurel <vincentmaurel82@gmail.com>
2018-12-22 15:01:33 -0500

    test deploy

--- startup/routes.js
+++ startup/routes.js
@@ -5,7 +5,8 @@ const error = require("../middleware/error");
 module.exports = function(app) {
   app.use(express.json());
   const cors = require("cors");
-  app.use(cors({ origin: "https://poc-react-member-search.herokuapp.com" }));
+  //app.use(cors({ origin: "https://poc-react-member-search.herokuapp.com" }));
+  app.use(cors({ origin: "http://localhost:3000" }));
   app.use("/api/members", members);
   app.use(error);
 };
